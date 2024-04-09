bdbd95ba80a72d05360072643b591497639ab29a
Prateek Suman <prateeksuman51@gmail.com>
2024-04-09 13:14:50 +0530

    nav and post button added

--- src/components/AsideLeft/AsideLeft.jsx
+++ src/components/AsideLeft/AsideLeft.jsx
@@ -1,5 +1,56 @@
+import { NavLink } from "react-router-dom";
+import { IoMdHeart, IoMdHome, IoMdCompass, IoMdBookmark } from "react-icons/io";
+
+import "./AsideLeft.css";
+
 const AsideLeft = () => {
-  return <div>AsideLeft</div>;
+  const activeLink = ({ isActive }) => ({
+    backgroundColor: isActive ? "rgb(215 228 255)" : "",
+  });
+  return (
+    <div className="aside-nav-container">
+      <nav className="aside-left-nav">
+        <NavLink to="/" className="aside-left-nav-link" style={activeLink}>
+          <div className="nav-icons">
+            <IoMdHome />
+          </div>
+          <div>Home</div>
+        </NavLink>
+        <NavLink
+          to="/explore"
+          className="aside-left-nav-link"
+          style={activeLink}
+        >
+          <div className="nav-icons">
+            <IoMdCompass />
+          </div>
+          <div>Explore</div>
+        </NavLink>
+        <NavLink
+          to="/bookmarks"
+          className="aside-left-nav-link"
+          style={activeLink}
+        >
+          <div className="nav-icons">
+            <IoMdBookmark />
+          </div>
+          <div>Bookmarks</div>
+        </NavLink>
+        <NavLink
+          to="/liked-post"
+          className="aside-left-nav-link"
+          style={activeLink}
+        >
+          <div className="nav-icons">
+            <IoMdHeart />
+          </div>
+          <div>Likes</div>
+        </NavLink>
+      </nav>
+
+      <button className="aside-left-post">Post</button>
+    </div>
+  );
 };
 
 export default AsideLeft;
